2bd33f6a7fdde39459f8ea8907554c534d35ceb4
Rafael Camacho <rafael@softwarecriollo.com>
2016-08-07 23:37:43 -0400

    user migrations changed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160803160333) do
+ActiveRecord::Schema.define(version: 20160808033313) do
 
   create_table "area_users", force: :cascade do |t|
     t.integer  "user_id",    limit: 4, null: false
@@ -45,6 +45,16 @@ ActiveRecord::Schema.define(version: 20160803160333) do
     t.datetime "updated_at"
   end
 
+  create_table "identities", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.string   "provider",   limit: 255
+    t.string   "uid",        limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "identities", ["user_id"], name: "index_identities_on_user_id", using: :btree
+
   create_table "places", force: :cascade do |t|
     t.string   "name",       limit: 255, null: false
     t.string   "address",    limit: 255, null: false
@@ -54,14 +64,29 @@ ActiveRecord::Schema.define(version: 20160803160333) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",       limit: 255, null: false
-    t.string   "last_name",  limit: 255, null: false
-    t.string   "email",      limit: 255, null: false
-    t.string   "avatar",     limit: 255, null: false
-    t.string   "profile",    limit: 255, null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "phone",      limit: 255, null: false
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "name",                   limit: 255,              null: false
+    t.string   "last_name",              limit: 255,              null: false
+    t.string   "avatar",                 limit: 255
+    t.string   "profile",                limit: 255,              null: false
+    t.string   "phone",                  limit: 255
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.string   "authentication_token",   limit: 30
   end
 
+  add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  add_foreign_key "identities", "users"
 end

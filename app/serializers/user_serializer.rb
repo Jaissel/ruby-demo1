371e48fcc1a4b605fb704b9f32c4eefc6404575d
Rafael Camacho <rafael@softwarecriollo.com>
2016-08-18 00:20:45 -0400

    profile industry and area added

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,15 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name,:last_name, :profile, :avatar, :profile, :phone, :email
+  attributes :id, :name,:last_name, :profile, :avatar, :profile, :phone, :email, :industry, :area
+
+  def industry
+    industry_area_user = object.industry_area_users.first
+    industry_area = IndustryArea.find(industry_area_user.industry_area_id)
+    IndustrySerializer.new(industry_area.industry).serializable_hash
+  end
+
+  def area
+    industry_area_user = object.industry_area_users.first
+    industry_area = IndustryArea.find(industry_area_user.industry_area_id)
+    AreaSerializer.new(industry_area.area).serializable_hash
+  end
 end

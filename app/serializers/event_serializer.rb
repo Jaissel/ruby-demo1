32568abdb5787d7dbd8c84053fe956427353883c
Rafael Camacho <rafael@softwarecriollo.com>
2016-08-17 23:33:38 -0400

    event date format fix

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,4 +1,8 @@
 class EventSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :schedule, :avatar, :cover, :capacity
   belongs_to :place
+
+  def schedule
+    object.schedule.strftime("%Y-%m-%d %H:%M")
+  end
 end

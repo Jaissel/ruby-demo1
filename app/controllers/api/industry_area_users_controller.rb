77b32dd39461a6ac1a907bfdd506688bcb4b852c
Rafael Camacho <rafael@softwarecriollo.com>
2016-10-17 15:29:09 -0400

    industry area users post, fix to create all at once

--- app/controllers/api/industry_area_users_controller.rb
+++ app/controllers/api/industry_area_users_controller.rb
@@ -1,19 +1,24 @@
 class Api::IndustryAreaUsersController < ApplicationController
   respond_to :json
+  before_action :find_or_create_industry_areas
 
   def create
-    @industry_area = IndustryArea.find_or_create_by(industry_id: params[:industry_id], area_id: params[:area_id])
-    @industry_area_users = IndustryAreaUser.find_or_initialize_by(user_id: params[:user_id], industry_area_id: @industry_area.id)
-    if @industry_area_users.save
-      render json: {
-                      success: true,
-                      response: @industry_area_users.to_json
-                    }
-    else
-      render json: {
-                      success: false,
-                      info: @industry_area_users.errors.to_json
-                    }
+    render json: {
+                    success: true,
+                    response: @industry_area_users.to_json
+                  }
+  end
+
+  private
+  def find_or_create_industry_areas
+    industry_areas = []
+    @industry_area_users = []
+    params[:industry_area].each do |industry_area_param|
+      industry_areas << IndustryArea.find_or_create_by(industry_id: industry_area_param[0], area_id: industry_area_param[1])
+    end
+
+    industry_areas.each do |industry_area|
+      @industry_area_users << IndustryAreaUser.find_or_create_by(user_id: params[:user_id], industry_area_id: industry_area.id)
     end
   end
 

16530dd83a73fc4daaeb657bf90a339abe02dbdd
Rafael Camacho <rafael@softwarecriollo.com>
2016-11-09 13:25:29 -0400

    validating event and user for the attendance

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -4,4 +4,5 @@ class Attendance < ActiveRecord::Base
   scope :attending, -> { where(status: true) }
   scope :not_attending, ->  { where(status: false) }
   scope :active_events, -> { joins(:event).where("events.schedule >= ?", DateTime.now ) }
+  validates_presence_of :user_id, :event_id
 end

c7ee2453b824629751424bd67b7f191825247975
Rafael Camacho <rafael@softwarecriollo.com>
2016-11-13 23:03:24 -0400

    industry areas form fix

--- app/admin/industry_area.rb
+++ app/admin/industry_area.rb
@@ -6,8 +6,8 @@ ActiveAdmin.register IndustryArea do
 
   form do |f|
     f.inputs "Industry Area Details" do
-      f.input :industry, :label => "Places", :include_blank => false, :as => :select, :collection => Place.all.collect {|p| [p.name, p.id]}
-      f.input :area, :label => "Places", :include_blank => false, :as => :select, :collection => Place.all.collect {|p| [p.name, p.id]}
+      f.input :industry, :label => "Industry", :include_blank => false, :as => :select, :collection => Industry.all.collect {|p| [p.name, p.id]}
+      f.input :area, :label => "Area", :include_blank => false, :as => :select, :collection => Area.all.collect {|p| [p.name, p.id]}
     end
     f.actions
    end

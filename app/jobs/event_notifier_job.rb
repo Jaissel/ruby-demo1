1f9d9d783615e92e8169a6e31d2d38811091c5ac
tony suarez <suarezcumare@gmail.com>
2017-02-16 08:56:19 -0400

    return event for id

--- app/jobs/event_notifier_job.rb
+++ app/jobs/event_notifier_job.rb
@@ -1,15 +1,14 @@
 class EventNotifierJob < ActiveJob::Base
+	discard_on ActiveJob::DeserializationError
   queue_as :default
 
-  def perform(current_event)
+  def perform(event_id)
 
-  	#current_event = Event.find(event_id)
-  	puts "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-  	puts current_event.to_json
+  	current_event = Event.find(event_id)
  
   	if !current_event.nil?  
-	  	NewMailer.delay_until(current_event.schedule.to_datetime + 4.hour - 1.day).remembering_event(current_event, "1 day for event: ")
-	  	NewMailer.delay_until(current_event.schedule.to_datetime + 4.hour - 2.hour).remembering_event(current_event, "2 hours for event: ")
+	  	NewMailer.delay_until(current_event.schedule.to_datetime - 1.day).remembering_event(event_id, "1 day for event: ")
+	  	NewMailer.delay_until(current_event.schedule.to_datetime - 2.hour).remembering_event(event_id, "2 hours for event: ")
 		end
   end
 

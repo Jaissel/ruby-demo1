2e5ce753b99022c1ba85aecfdccb93e4d3bef78f
tony suarez <suarezcumare@gmail.com>
2017-02-16 10:03:23 -0400

    show date from email

--- app/jobs/event_notifier_job.rb
+++ app/jobs/event_notifier_job.rb
@@ -6,8 +6,12 @@ class EventNotifierJob < ActiveJob::Base
   	current_event = Event.find(event_id)
  
   	if !current_event.nil?  
-	  	NewMailer.delay_until(current_event.schedule.to_datetime - 1.day).remembering_event(event_id, "1 day for event: ")
-	  	NewMailer.delay_until(current_event.schedule.to_datetime - 2.hour).remembering_event(event_id, "2 hours for event: ")
+  		puts "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+  		puts current_event.schedule.to_datetime + 4.hour - 1.day
+  		puts current_event.schedule.to_datetime + 4.hour - 2.hour
+  		puts "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"
+	  	NewMailer.delay_until(current_event.schedule.to_datetime + 4.hour - 1.day).remembering_event(event_id, "1 day for event: ")
+	  	NewMailer.delay_until(current_event.schedule.to_datetime + 4.hour - 2.hour).remembering_event(event_id, "2 hours for event: ")
 		end
   end
 

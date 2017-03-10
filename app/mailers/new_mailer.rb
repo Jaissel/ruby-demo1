884525f66e1ec0a1ee75e5ab050dc857ab805535
tony suarez <suarezcumare@gmail.com>
2017-03-10 18:22:23 -0400

    type content

--- app/mailers/new_mailer.rb
+++ app/mailers/new_mailer.rb
@@ -17,7 +17,9 @@ default from: 'info@nwmeeting.com'
     @user = user
     @url = Rails.env.production? ? 'http://nwmeeting.com/' : 'http://localhost:3000/'
     @url += "password/edit.#{@user.id}?reset_password_token=#{@user.reset_password_token}"
+    content_type "text/html"
     mail(to: @user.email, subject: 'Reset Password.')
+
   end
 
   def remembering_event(event_id, message)

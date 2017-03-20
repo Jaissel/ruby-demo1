ed5b958ee3e801d05b9c4a04b967c1841a1a417e
tony suarez <suarezcumare@gmail.com>
2017-03-20 07:26:54 -0400

    puts

--- app/jobs/event_notifier_job.rb
+++ app/jobs/event_notifier_job.rb
@@ -7,8 +7,8 @@ class EventNotifierJob < ActiveJob::Base
  
   	if !current_event.nil?  
   		puts "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-  		puts current_event.schedule.to_datetime + 5.hour - 1.day
-  		puts current_event.schedule.to_datetime + 5.hour - 2.hour
+  		puts current_event.schedule.to_datetime - 1.day
+  		puts current_event.schedule.to_datetime - 2.hour
   		puts "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"
 	  	NewMailer.delay_until(current_event.schedule.to_datetime - 1.day).remembering_event(event_id, "")
 	  	NewMailer.delay_until(current_event.schedule.to_datetime - 2.hour).remembering_event(event_id, "")

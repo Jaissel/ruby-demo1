33d08d3741cc2f4f54160ae2d598f00c1ddba585
tony suarez <suarezcumare@gmail.com>
2017-04-17 23:31:43 -0300

    fix error all areas

--- app/controllers/api/areas_controller.rb
+++ app/controllers/api/areas_controller.rb
@@ -2,7 +2,8 @@ class Api::AreasController < ApplicationController
   respond_to :json
 
   def index
-    @areas = IndustryArea.all.map { |i| IndustryAreaSerializer.new(i).serializable_hash }
+
+    @areas = IndustryArea.all.map { |i| IndustryAreaSerializer.new(i).serializable_hash if ! i.area.nil?}.compact
     render json: {
                     success: true,
                     response: @areas

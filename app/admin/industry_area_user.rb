c52c1a7180146da498bdee7323c46c32a6ab3d9e
tony suarez <suarezcumare@gmail.com>
2017-05-07 22:10:17 -0300

    industria areas@

--- app/admin/industry_area_user.rb
+++ app/admin/industry_area_user.rb
@@ -5,12 +5,22 @@ ActiveAdmin.register IndustryAreaUser do
   end
   permit_params :user_id, :industry_area_id
 
+    index do
+    selectable_column
+    id_column
+    column :user_id
+    column :industry_area_id
+    actions
+  end
+
 
   form do |f|
+    
     f.inputs "Industry Area Details" do
       f.input :industry_area_id, :label => "Industry Area", :include_blank => false, :as => :select, :collection => IndustryArea.all.collect {|p| [p.industry_area.name, p.id]}
       f.input :user_id, :label => "User", :include_blank => false, :as => :select, :collection => User.all.collect {|p| [p.name, p.id]}
     end
+
     f.actions
    end
 end

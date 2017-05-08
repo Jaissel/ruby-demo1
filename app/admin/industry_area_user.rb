eefe764e375ccd8a84ba110feba42006e6e60ef5
tony suarez <suarezcumare@gmail.com>
2017-05-07 21:55:01 -0300

    industria areas@

---
+++ app/admin/industry_area_user.rb
@@ -0,0 +1,15 @@
+ActiveAdmin.register IndustryAreaUser do
+  menu parent: 'industry_areas'
+  controller do
+    skip_before_filter :authenticate_user!
+  end
+  permit_params :user_id, :industry_area_id
+
+  form do |f|
+    f.inputs "Industry Area Details" do
+      f.input :industry_area_id, :label => "Industry Area", :include_blank => false, :as => :select, :collection => IndustryArea.all.collect {|p| [p.industry_area.name, p.id]}
+      f.input :user_id, :label => "User", :include_blank => false, :as => :select, :collection => User.all.collect {|p| [p.name, p.id]}
+    end
+    f.actions
+   end
+end

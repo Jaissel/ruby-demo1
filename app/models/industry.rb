7a62e98ffb4d7dcca23f935674fc45ad1661f04d
lessiaj <jaissel.perez@elitgon.com>
2017-05-13 21:58:35 -0400

    controladores (no eliminado aun industry_area)

--- app/models/industry.rb
+++ app/models/industry.rb
@@ -2,11 +2,11 @@ class Industry < ActiveRecord::Base
   #has_many :industry_industrys, dependent: :destroy
   #has_many :industrys, through: :industry_industrys
   
-  has_many :industry_users, dependent: :destroy
-  has_many :users, through: :industry_users
+  has_many :industry_user, dependent: :destroy
+  has_many :users, through: :industry_user
 
   has_many :industry_event, dependent: :destroy
-  has_many :events, through: :industry_events
+  has_many :events, through: :industry_event
 
   validates_presence_of :name
   

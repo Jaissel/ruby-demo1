80e16ce29694d648af0b25eacaefdf77b77a96c0
lessiaj <jaissel.perez@elitgon.com>
2017-05-18 21:02:04 -0400

    modificaciones

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,12 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name,:last_name, :profile, :avatar, :profile, :location, :company, :position, :phone, :email, :industries, :areas,
-
-  def industries
-    @user_industries = []
-    object.industry_users.map{|i| i.industry_user.industry_id}.uniq{ |id| id }.each do |industry_id|
-      @user_industries << IndustrySerializer.new(Industry.find(industry_id)).serializable_hash
-    end
-    @user_industries
-  end 
-
+  attributes :id, :name,:last_name, :profile, :avatar, :profile, :location, :company, :position, :phone, :email, :industries, :areas
 end

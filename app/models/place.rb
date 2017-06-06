b95c41fa5027477ff59595aa4474d8ddb532b39a
lessiaj <jaissel.perez@elitgon.com>
2017-06-06 11:11:54 -0400

    palces

--- app/models/place.rb
+++ app/models/place.rb
@@ -4,11 +4,11 @@ class Place < ActiveRecord::Base
   has_many :events
   has_many :place_emails
   belongs_to :city
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, 
-          :presence => {:message => "Enter your email address!" },
-          :format => { :with => VALID_EMAIL_REGEX, :message => "Enter a valid Email address !"}
-  validates_presence_of :name, :address, :latitude, :longitude
+  #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+#	validates :email, 
+#          :presence => {:message => "Enter your email address!" },
+#          :format => { :with => VALID_EMAIL_REGEX, :message => "Enter a valid Email address !"}
+#  validates_presence_of :name, :address, :latitude, :longitude
 
   
 end
